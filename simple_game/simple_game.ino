48a0e6cd378a23f1a3a237bf9c7fb7684411a3d0
Patrick Taylor <taylor.patrick@charterschool.org>
2018-03-06 09:00:42 -0500

    game work

--- simple_game/simple_game.ino
+++ simple_game/simple_game.ino
@@ -1,8 +1,10 @@
 int correctValue =0;
 int acceptableRange = 50;
 int turnLimit = 350;
+boolean unlockable = false;
 void setup() {
   // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
   correctValue =  random(50, 900);
   Serial.begin(9600);
   randomSeed(analogRead(0));
@@ -16,6 +18,15 @@ void loop() {
   int correctnessValue = abs(tempValue);
   if(correctnessValue <= acceptableRange){
     turnLimit = 130;
+    unlockable = true;
+  } else {
+    int howOff = correctnessValue - acceptableRange;
+    turnLimit = map(howOff, 1, 900, 150, 300);
+  }
+  if(wrenchValue < turnLimit){
+    digitalWrite(8, HIGH);
+  } else {
+    digitalWrite(8, LOW);
   }
   Serial.print(lockpickValue);
   Serial.print("\r\n");
